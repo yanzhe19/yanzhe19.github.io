04d68aaedab082a41830e86acb3030831b307a40
yanzhe19 <yanzhe19@gmail.com>
2015-01-29 22:08:55 -0500

    FindTheTreasure web game added

---
+++ FindTheTreasure/js/game.js
@@ -0,0 +1,8 @@
+﻿/*Source  file  name: game.js,
+Author's name:   Zhe Yan ,
+Last  Modified  by:   Zhe Yan ,
+Date  last  Modified:   2015_01_29 ,
+Program  description：  This is the first assignment of COMP397  –  Web  Game  Programming。 It is a simple selection game which aske
+    player find the treasure in this area. There are 8 outcomes in total and only one is positive outcome.
+File Description:   This file is the javascript file of this game. Current there is not code in this file.
+Revision  History:   version 1.0  */
