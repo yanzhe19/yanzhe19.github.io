4cb5f0cccabb7d9cb52a2bca2b66f4fbf8c2fdcb
yanzhe19 <yanzhe19@gmail.com>
2015-02-27 21:21:05 -0500

    add mobile version of portfolio site
    Use JQuery Mobile

---
+++ MobileSite/Scripts/mobile.js
@@ -0,0 +1,6 @@
+﻿//File  name: mobile.js,  Author's  name: Zhe Yan (300706310),
+//web  site  name: Zhe's portfolio mobile version, 
+//file  description: this is the javascript file for mobile portfolio website.
+function init() {
+    console.log("mobile app has started...");
+}
